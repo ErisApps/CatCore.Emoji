3687812960c784073b2c8383ce8be1b9be1558d0
Eris <erisapps@outlook.be>
2021-11-28 15:00:11 +0100

    💚 Fix CI Build

--- CatCore.Emoji.Models/EmojiTreeRoot.cs
+++ CatCore.Emoji.Models/EmojiTreeRoot.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("CatCore.Emoji.SourceGeneration.GNoto")]
+[assembly: InternalsVisibleTo("CatCore.Emoji.SourceGeneration.Twemoji")]
 namespace CatCore.Emoji.Models
 {
 	public class EmojiTreeRoot : EmojiTreeNodeBase
