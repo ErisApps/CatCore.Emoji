57f6c2b798912488fcd3d9b609b4744ae7e61f02
Eris <erisapps@outlook.be>
2023-02-13 21:39:18 +0100

    🚑️ Change Twemoji CDN url

--- CatCore.Emoji.SourceGeneration.Twemoji/TwemojiDataSourceGenerator.cs
+++ CatCore.Emoji.SourceGeneration.Twemoji/TwemojiDataSourceGenerator.cs
@@ -7,7 +7,7 @@ namespace CatCore.Emoji.SourceGeneration.Twemoji
 	public class TwemojiDataSourceGenerator : EmojiDataSourceGeneratorBase
 	{
 		protected override string Type => nameof(Twemoji);
-		protected override string BaseUrl => "https://twemoji.maxcdn.com/v/latest/72x72/";
+		protected override string BaseUrl => "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/72x72/";
 		protected override string CodepointSeparator => "-";
 	}
 }
